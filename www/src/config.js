65fb05c4606febd7e794abfe2bfb380ce5a74521
Amit Evron <amit@wing.security>
2021-07-04 19:05:43 +0300

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "1qsvt75f6rrclb8nhsqiuoupbe",     // CognitoClientID
+  "api_base_url": "https://6gjcynira7.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "todoappdemo-lambda-refarch-webapp.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1t0d0it1dreuz.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
